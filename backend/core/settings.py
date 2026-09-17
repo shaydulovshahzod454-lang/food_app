527c573d50911045d16905569dd5422512c4a417
Shahzod <shaydulovshahzod454@gmail.com>
2026-09-17 14:30:07 +0500

    Add PostgreSQL configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant_db',
+        'USER': 'postgres',
+        'PASSWORD': 'shahzod2007',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
